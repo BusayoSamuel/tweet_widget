bd779f80ad9e8be87127cc04af30768363289d78
BusayoSamuel <busayosalisu@gmail.com>
2024-02-17 13:25:55 +0000

    enabling home screen widgets on IOS

---
+++ ios/tweetWidget/tweetWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  tweetWidgetBundle.swift
+//  tweetWidget
+//
+//  Created by admin on 2/17/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct tweetWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        tweetWidget()
+    }
+}
